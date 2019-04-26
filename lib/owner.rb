b3139c4a84ae253e39e65d5c78c5010d324e06fa
Ricartho Franck <ricartho.franck@esih.edu>
2019-04-26 16:58:11 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -60,4 +60,9 @@ class Owner
     end
     pets.clear
   end
+  
+  def list_pets
+     "I have #{pets[:fishes].length} fish, #{pets[:dogs].length} dog(s), and #{pets[:cats].length} cat(s)."
+  end
+  
 end
